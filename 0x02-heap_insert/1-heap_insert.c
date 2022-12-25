d0c19ee7625eeb7abd081c888711b0a0c14094a2
Astambar <leuchartjason@gmail.com>
2022-12-25 21:19:24 +0000

    je prie pour que le bug git passe

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -12,40 +12,51 @@
 
 heap_t *heap_insert(heap_t **root, int value)
 {
-heap_t *node = NULL;
-heap_t *temp = NULL;
+    heap_t *node = NULL;
+    heap_t *temp = NULL;
 
-if (!root)
-	return (NULL);
+    if (!root)
+        return (NULL);
 
-node = malloc(sizeof(*node));
-if (!node)
-	return (NULL);
+    node = malloc(sizeof(*node));
+    if (!node)
+        return (NULL);
 
-node->n = value;
-node->left = node->right = NULL;
-if (!*root)
-{
-	*root = node;
-	return (node);
+    node->n = value;
+    node->left = node->right = NULL;
+    if (!*root)
+    {
+        *root = node;
+        return (node);
+    }
+
+    temp = *root;
+    while (temp)
+    {
+        if (temp->left && !temp->right)
+        {
+            temp->right = node;
+            break;
+        }
+        else if (!temp->left)
+        {
+            temp->left = node;
+            break;
+        }
+        temp = temp->left;
+    }
+    node->parent = temp;
+
+    /* Vérifie que le nouveau noeud respecte la propriété de tas binaire */
+    heap_t *current = node;
+    while (current->parent && current->n > current->parent->n)
+    {
+        int temp = current->n;
+        current->n = current->parent->n;
+        current->parent->n = temp;
+        current = current->parent;
+    }
+
+    return (node);
 }
 
-temp = *root;
-while (temp)
-{
-	if (temp->left && !temp->right)
-	{
-		temp->right = node;
-		break;
-	}
-	else if (!temp->left)
-	{
-		temp->left = node;
-		break;
-	}
-	temp = temp->left;
-}
-node->parent = temp;
-
-return (node);
-}
