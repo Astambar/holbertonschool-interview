24506b81cff857c7423e7d67ce7e20651dd32e1f
Astambar <leuchartjason@gmail.com>
2022-12-25 21:30:33 +0000

    je prie pour que le bug git passe

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -12,51 +12,67 @@
 
 heap_t *heap_insert(heap_t **root, int value)
 {
-    heap_t *node = NULL;
-    heap_t *temp = NULL;
+heap_t *node = NULL;
+heap_t *temp = NULL;
 
-    if (!root)
-        return (NULL);
+if (!root)
+	return (NULL);
 
-    node = malloc(sizeof(*node));
-    if (!node)
-        return (NULL);
+node = malloc(sizeof(*node));
+if (!node)
+	return (NULL);
 
-    node->n = value;
-    node->left = node->right = NULL;
-    if (!*root)
-    {
-        *root = node;
-        return (node);
-    }
-
-    temp = *root;
-    while (temp)
-    {
-        if (temp->left && !temp->right)
-        {
-            temp->right = node;
-            break;
-        }
-        else if (!temp->left)
-        {
-            temp->left = node;
-            break;
-        }
-        temp = temp->left;
-    }
-    node->parent = temp;
-
-    /* Vérifie que le nouveau noeud respecte la propriété de tas binaire */
-    heap_t *current = node;
-    while (current->parent && current->n > current->parent->n)
-    {
-        int temp = current->n;
-        current->n = current->parent->n;
-        current->parent->n = temp;
-        current = current->parent;
-    }
-
-    return (node);
+node->n = value;
+node->left = node->right = NULL;
+if (!*root)
+{
+	*root = node;
+	return (node);
 }
 
+temp = *root;
+while (temp)
+{
+	if (temp->left && !temp->right)
+	{
+		temp->right = node;
+		break;
+	}
+	else if (!temp->left)
+	{
+		temp->left = node;
+		break;
+	}
+	temp = temp->left;
+}
+node->parent = temp;
+
+/* Vérifie que le nouveau noeud respecte la propriété de tas binaire */
+heap_t *current = node;
+check_heap_property(current);
+
+return (node);
+}
+
+/**
+ * check_heap_property - Check that the value of a node
+ * is greater than or equal to its parent
+ *
+ * @current: Pointer to the node to check
+ *
+ * If the value of the node
+ * is greater than that of its parent, swap the two nodes.
+ * Repeat until the heap property is satisfied.
+ */
+
+void check_heap_property(heap_t *current)
+{
+while (current->parent && current->n > current->parent->n)
+{
+	int temp = current->n;
+
+	current->n = current->parent->n;
+	current->parent->n = temp;
+	current = current->parent;
+}
+}
