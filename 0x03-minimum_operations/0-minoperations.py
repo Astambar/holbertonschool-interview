631cfbbe48af7315221a7517152bb726dccc0dbf
Astambar <leuchartjason@gmail.com>
2023-01-05 21:36:08 +0000

    project retail

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -5,18 +5,20 @@ file creation minOperations
 
 
 def minOperations(n):
-    """minOperation"""
+    """
+    method minOperations
+    """
     if n == 1 or n == 0:
         return 0
 
-    total = 0
-    for i in range(2, n+1):
-        found_factor = False
-        while n % i == 0:
-            found_factor = True
-            n = n / i
-            total += i
-        if not found_factor:
-            break
-    return total
+    init = 2
+    count = 0
+
+    while n > 1:
+        if n % init == 0:
+            n = n / init
+            count += init
+        else:
+            init += 1
+    return count
 
