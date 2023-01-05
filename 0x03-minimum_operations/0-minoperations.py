397c076796417d0d9b96588dc97e1b8c3eb80f46
Astambar <leuchartjason@gmail.com>
2023-01-05 21:24:22 +0000

    project heap

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -4,13 +4,10 @@ def minOperations(n):
     fonction minOperation
     """
     operations = 0
-    # Set the current number of characters in the file to 1
     current_characters = 1
     while current_characters < n:
-        # Paste the current contents of the file
         current_characters *= 2
         operations += 1
     if current_characters > n:
-        # Copy all the characters in the file
         operations += 1
     return operations
