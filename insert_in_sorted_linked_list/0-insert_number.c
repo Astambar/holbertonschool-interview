169115194b147ae6272f99e11d49b61e8e1e12bc
Astambar <leuchartjason@gmail.com>
2023-03-30 12:47:19 +0000

    correction betty style

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -2,22 +2,25 @@
 #include <stdlib.h>
 #include "lists.h"
 
+
 /**
- * find_insertion_point - Trouve le point d'insertion pour un nouveau noeud dans une liste chaînée triée
+ * find_insertion_point - Trouve le point d'insertion
+ * pour un nouveau noeud dans une liste chaînée triée
  * @head: Pointeur vers la tête de la liste chaînée
  * @number: La valeur à insérer dans le nouveau noeud
- * Return: un double pointeur vers le nœud précédent où le nouveau nœud doit être inséré
+ * Return: un double pointeur vers le nœud précédent
+ * où le nouveau nœud doit être inséré
  */
 listint_t **find_insertion_point(listint_t **head, int number)
 {
-    listint_t **current_node = head;
+	listint_t **current_node = head;
 
-    while (*current_node && number > (*current_node)->n)
-    {
-        current_node = &(*current_node)->next;
-    }
+	while (*current_node && number > (*current_node)->n)
+	{
+		current_node = &(*current_node)->next;
+	}
 
-    return current_node;
+	return (current_node);
 }
 
 /**
@@ -28,35 +31,37 @@ listint_t **find_insertion_point(listint_t **head, int number)
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *new_node;
-    listint_t **insertion_point;
+	listint_t *new_node;
+	listint_t **insertion_point;
 
-    new_node = malloc(sizeof(listint_t));
-    if (!new_node)
-        return NULL;
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
 
-    new_node->n = number;
-    new_node->next = NULL;
+	new_node->n = number;
+	new_node->next = NULL;
 
-    if (!*head)
-    {
-        *head = new_node;
-        return new_node;
-    }
+	if (!*head)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 
-    insertion_point = find_insertion_point(head, number);
-    insert_node_at_point(insertion_point, new_node);
+	insertion_point = find_insertion_point(head, number);
+	insert_node_at_point(insertion_point, new_node);
 
-    return new_node;
+	return (new_node);
 }
 
 /**
- * insert_node_at_point - Insère un nouveau noeud dans une liste chaînée à l'emplacement spécifié
- * @insertion_point: Double pointeur vers le nœud précédent où le nouveau nœud doit être inséré
+ * insert_node_at_point - Insère un nouveau noeud
+ * dans une liste chaînée à l'emplacement spécifié
+ * @insertion_point: Double pointeur vers le nœud précédent
+ * où le nouveau nœud doit être inséré
  * @new_node: Pointeur vers le nouveau noeud à insérer
  */
 void insert_node_at_point(listint_t **insertion_point, listint_t *new_node)
 {
-    new_node->next = *insertion_point;
-    *insertion_point = new_node;
+	new_node->next = *insertion_point;
+	*insertion_point = new_node;
 }
