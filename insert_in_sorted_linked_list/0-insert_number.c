807e7a9d88ed70930337b61f9d4ec738b0bb67d8
Astambar <leuchartjason@gmail.com>
2023-03-30 13:33:05 +0000

    améliore comment

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include "lists.h"
 
-
 /**
  * get_previous_node_for_insertion - Trouve le point d'insertion
  * pour un nouveau noeud dans une liste chaînée triée
@@ -13,6 +12,7 @@
  */
 listint_t **get_previous_node_for_insertion(listint_t **head, int number)
 {
+	/* Pointe vers la tête de la liste chaînée */
 	listint_t **current_node = head;
 
 	while (*current_node && number > (*current_node)->n)
@@ -31,25 +31,33 @@ listint_t **get_previous_node_for_insertion(listint_t **head, int number)
  */
 listint_t *insert_node(listint_t **head, int number)
 {
+	/* Alloue de l'espace pour le nouveau noeud */
 	listint_t *new_node;
+	/* Pointe vers l'endroit où insérer le nouveau noeud */
 	listint_t **insertion_point;
 
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 		return (NULL);
 
+	/* Initialise le nouveau noeud */
 	new_node->n = number;
 	new_node->next = NULL;
 
+	/* Si la liste est vide, ajoute le nouveau noeud en tant que tête */
 	if (!*head)
 	{
 		*head = new_node;
 		return (new_node);
 	}
 
+	/* Trouve l'endroit où insérer le nouveau noeud */
 	insertion_point = get_previous_node_for_insertion(head, number);
+
+	/* Insère le nouveau noeud avant le point d'insertion */
 	insert_node_before(insertion_point, new_node);
 
+	/* Retourne le pointeur vers le nouveau noeud créé */
 	return (new_node);
 }
 
@@ -62,6 +70,7 @@ listint_t *insert_node(listint_t **head, int number)
  */
 void insert_node_before(listint_t **insertion_point, listint_t *new_node)
 {
+	/* Insère le nouveau noeud avant le point d'insertion */
 	new_node->next = *insertion_point;
 	*insertion_point = new_node;
 }
