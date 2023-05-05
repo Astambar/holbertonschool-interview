d5df8bcd74d68489cbb5e2158bf9a91de593cab8
Astambar <leuchartjason@gmail.com>
2023-05-05 19:44:28 +0000

    new version test

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,48 +1,61 @@
 #!/usr/bin/python3
-"""Log parsing."""
+"""log_parsing"""
 
 import sys
 
-TOTAL_SIZE = 0
-STATUS_CODES = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
-LINE_COUNT = 0
+
+def parse_line(line, total_size, status_codes):
+    """Parse une ligne de log"""
+    data = line.split()
+    try:
+        size = int(data[-1])
+        total_size += size
+    except ValueError:
+        pass
+    try:
+        status = data[-2]
+        if status in status_codes:
+            status_codes[status] += 1
+    except IndexError:
+        pass
+    return total_size, status_codes
 
 
-def print_stats():
-    """Print statistics."""
-    print("File size: {}".format(TOTAL_SIZE))
-    for code in sorted(STATUS_CODES.keys()):
-        if STATUS_CODES[code] > 0:
-            print("{}: {}".format(code, STATUS_CODES[code]))
+def print_stats(total_size, status_codes):
+    """Affiche les statistiques"""
+    print("File size: {}".format(total_size))
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print("{}: {}".format(code, status_codes[code]))
 
 
-try:
-    for line in sys.stdin:
-        LINE_COUNT += 1
-        data = line.split()
-        try:
-            size = int(data[-1])
-            TOTAL_SIZE += size
-        except ValueError:
-            pass
-        try:
-            status = data[-2]
-            if status in STATUS_CODES:
-                STATUS_CODES[status] += 1
-        except IndexError:
-            pass
-        if LINE_COUNT % 10 == 0:
-            print_stats()
-    print_stats()
-except KeyboardInterrupt:
-    print_stats()
-    raise
+def parse_logs():
+    """Parse les logs depuis stdin"""
+    total_size = 0
+    status_codes = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0
+    }
+    line_count = 0
+    try:
+        for line in sys.stdin:
+            line_count += 1
+            total_size, status_codes = parse_line(line,
+                                                  total_size,
+                                                  status_codes)
+            if line_count % 10 == 0:
+                print_stats(total_size, status_codes)
+        print_stats(total_size, status_codes)
+    except KeyboardInterrupt:
+        print_stats(total_size, status_codes)
+        raise
+
+
+if __name__ == '__main__':
+    parse_logs()
