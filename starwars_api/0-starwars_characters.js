f24467ea496b365c7eb2e553893f57f122ef0372
Astambar <leuchartjason@gmail.com>
2023-07-14 20:39:50 +0000

    add shebang

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,31 +1,38 @@
 #!/usr/bin/node
+/*
+ * Script pour afficher les noms des personnages d'un film Star Wars spécifié.
+ * L'ID du film est passé en tant que premier argument de ligne de commande.
+ * Ce script utilise l'API Star Wars (https://swapi.dev/) pour récupérer les données du film et des personnages.
+ * Il utilise le module `request` pour effectuer des requêtes HTTP à l'API.
+ */
 
 const request = require('request');
-const movieId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
 
-const url = `https://swapi.dev/api/films/${movieId}/`;
-
-function getCharacterName(url) {
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        const character = JSON.parse(body);
-        resolve(character.name);
-      }
-    });
-  });
-}
-
-request(url, async (error, response, body) => {
+// Effectue une requête à l'URL du film pour récupérer les données du film
+request(url, async function (error, response, body) {
   if (error) {
+    // En cas d'erreur, affiche l'erreur
     console.log(error);
   } else {
-    const film = JSON.parse(body);
-    for (const characterUrl of film.characters) {
-      const characterName = await getCharacterName(characterUrl);
-      console.log(characterName);
+    // Récupère la liste des personnages du film
+    const characters = JSON.parse(body).characters;
+    // Parcourt la liste des personnages
+    for (const c in characters) {
+      // Pour chaque personnage, effectue une requête à son URL pour récupérer ses données
+      const response = await new Promise((resolve, reject) => {
+        request(characters[c], (error, response, body) => {
+          if (error) {
+            // En cas d'erreur, rejette la promesse avec l'erreur
+            reject(error);
+          } else {
+            // Sinon, résout la promesse avec le nom du personnage
+            resolve(JSON.parse(body).name);
+          }
+        });
+      });
+      // Affiche le nom du personnage
+      console.log(response);
     }
   }
 });
