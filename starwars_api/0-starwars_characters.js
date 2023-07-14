0efdb7077726d2f43e1fd0fb096df8bcb544f780
Astambar <leuchartjason@gmail.com>
2023-07-14 20:34:40 +0000

    project starwars

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,29 @@
+const request = require('request');
+const movieId = process.argv[2];
+
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+function getCharacterName(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        const character = JSON.parse(body);
+        resolve(character.name);
+      }
+    });
+  });
+}
+
+request(url, async (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const film = JSON.parse(body);
+    for (const characterUrl of film.characters) {
+      const characterName = await getCharacterName(characterUrl);
+      console.log(characterName);
+    }
+  }
+});
