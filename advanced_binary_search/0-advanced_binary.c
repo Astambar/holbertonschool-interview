0bfb34e69ac185d8c50e57c685f803fe3859382b
Astambar <leuchartjason@gmail.com>
2023-08-04 12:02:59 +0000

    advanced_binary_search no loop print patch

--- advanced_binary_search/0-advanced_binary.c
+++ advanced_binary_search/0-advanced_binary.c
@@ -3,10 +3,64 @@
 #include "search_algos.h"
 
 /**
- * advanced_binary - Recherche une valeur dans un tableau trié d'entiers
+ * recursive_print_array - Fonction récursive pour afficher le tableau.
  *
- * @array: Un pointeur vers le premier élément du tableau dans
- * lequel effectuer la recherche.
+ * @array: Un pointeur vers le premier élément du tableau.
+ * @left: L'index de gauche du sous-tableau actuel à afficher.
+ * @right: L'index de droite du sous-tableau actuel à afficher.
+ */
+void recursive_print_array(int *array, size_t left, size_t right)
+{
+    if (left <= right)
+    {
+        printf("%d", array[left]);
+        if (left < right)
+            printf(", ");
+        recursive_print_array(array, left + 1, right);
+    }
+}
+
+/**
+ * advanced_binary_recursive - Fonction auxiliaire récursive pour advanced_binary.
+ *
+ * @array: Un pointeur vers le premier élément du tableau.
+ * @left: L'index de gauche du sous-tableau actuel dans lequel effectuer la recherche.
+ * @right: L'index de droite du sous-tableau actuel dans lequel effectuer la recherche.
+ * @value: La valeur à rechercher.
+ *
+ * Return: L'index où se trouve la valeur, ou -1 s'il n'est pas trouvé.
+ */
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
+{
+    if (array == NULL || left > right)
+        return (-1);
+
+    printf("Searching in array: ");
+    recursive_print_array(array, left, right);
+    printf("\n");
+
+    size_t mid = (left + right) / 2;
+
+    if (array[mid] == value)
+    {
+        if (mid == 0 || array[mid - 1] != value)
+            return (mid);
+        return advanced_binary_recursive(array, left, mid, value);
+    }
+    else if (array[mid] < value)
+    {
+        return advanced_binary_recursive(array, mid + 1, right, value);
+    }
+    else
+    {
+        return advanced_binary_recursive(array, left, mid, value);
+    }
+}
+
+/**
+ * advanced_binary - Recherche une valeur dans un tableau trié d'entiers.
+ *
+ * @array: Un pointeur vers le premier élément du tableau dans lequel effectuer la recherche.
  * @size: Le nombre d'éléments dans le tableau.
  * @value: La valeur à rechercher.
  *
@@ -14,47 +68,5 @@
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-    size_t left, right, mid;
-
-    if (array == NULL || size == 0)
-        return (-1);
-
-    left = 0;
-    right = size - 1;
-
-    while (left <= right)
-    {
-        print_array(array, left, right); // Print the array (no loop)
-
-        mid = (left + right) / 2;
-
-        if (array[mid] == value)
-        {
-            if (mid == 0 || array[mid - 1] != value)
-                return (mid);
-            right = mid - 1;
-        }
-        else if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid;
-    }
-
-    return (-1);
-}
-
-/* Recursive function to print the array */
-void print_array(int *array, size_t left, size_t right)
-{
-    printf("Searching in array: ");
-    if (left < right)
-    {
-        for (size_t i = left; i < right; i++)
-            printf("%d, ", array[i]);
-        printf("%d\n", array[right]);
-    }
-    else if (left == right)
-    {
-        printf("%d\n", array[left]);
-    }
+    return advanced_binary_recursive(array, 0, size - 1, value);
 }
