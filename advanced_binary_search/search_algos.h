0bbd293de594b5cd13a22cd51e204628adb31c14
Astambar <leuchartjason@gmail.com>
2023-08-04 12:04:56 +0000

    advanced_binary_search no loop print patch

--- advanced_binary_search/search_algos.h
+++ advanced_binary_search/search_algos.h
@@ -4,7 +4,8 @@
 #include <stddef.h>
 
 /* Function prototype for recursive array printing */
-void print_array(int *array, size_t left, size_t right);
+void recursive_print_array(int *array, size_t left, size_t right);
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
 /* Function prototype */
 int advanced_binary(int *array, size_t size, int value);
 
