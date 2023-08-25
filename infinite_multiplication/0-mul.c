bbf82f0311724069e61be316013af82dfd4f11ce
Astambar <leuchartjason@gmail.com>
2023-08-25 21:00:37 +0000

    correctif betty long function

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -73,6 +73,52 @@ void print_result(char *result)
 }
 
 /**
+ * calculateAndPrintResult - Calcule et affiche le résultat final.
+ * @num1: Le premier nombre.
+ * @num2: Le deuxième nombre.
+ */
+void calculateAndPrintResult(const char *num1, const char *num2)
+{
+	int length_num1 = strlen(num1);
+	int length_num2 = strlen(num2);
+	int total_length = length_num1 + length_num2;
+	char *result = _callocX(total_length);
+	int i, j, multiplication, carry_multiply,
+	addition, carry_addition, result_index;
+	int leading_zeros;
+
+	for (i = length_num2 - 1; i >= 0; i--)
+	{
+		carry_multiply = 0, carry_addition = 0;
+		for (j = length_num1 - 1; j >= 0; j--)
+		{
+			result_index = i + j + 1;
+			multiplication = (num1[j] - '0') * (num2[i] - '0') + carry_multiply;
+			carry_multiply = multiplication / 10;
+			addition = (result[result_index] - '0') + (multiplication % 10);
+			addition += carry_addition;
+			carry_addition = addition / 10;
+			result[result_index] = (addition % 10) + '0';
+		}
+		result[result_index - 1] = (carry_multiply + carry_addition) + '0';
+	}
+
+	leading_zeros = 0;
+	while (result[leading_zeros] == '0' && leading_zeros < total_length - 1)
+	{
+		leading_zeros++;
+	}
+
+	for (i = leading_zeros; i < total_length; i++)
+	{
+		printf("%c", result[i]);
+	}
+	printf("\n");
+
+	free(result);
+}
+
+/**
  * main - Point d'entrée du programme.
  * @argc: Le nombre d'arguments en ligne de commande.
  * @argv: Un tableau contenant les arguments en ligne de commande.
@@ -80,53 +126,19 @@ void print_result(char *result)
  */
 int main(int argc, char **argv)
 {
-    int i, j, length_num1, length_num2, total_length,
-	multiplication, addition, carry_multiply, carry_addition,
-	result_index, leading_zeros = 0;
-    char *result;
+	if (argc != 3 || !_isnumber(argv[1]) || !_isnumber(argv[2]))
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    if (argc != 3 || !_isnumber(argv[1]) || !_isnumber(argv[2]))
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (atoi(argv[1]) == 0 || atoi(argv[2]) == 0)
+	{
+		printf("0\n");
+		exit(0);
+	}
 
-    if (atoi(argv[1]) == 0 || atoi(argv[2]) == 0)
-    {
-        printf("0\n");
-        exit(0);
-    }
+	calculateAndPrintResult(argv[1], argv[2]);
 
-    length_num1 = strlen(argv[1]);
-    length_num2 = strlen(argv[2]);
-    total_length = length_num1 + length_num2;
-    result = _callocX(total_length);
-
-    for (i = length_num2 - 1; i >= 0; i--)
-    {
-        carry_multiply = 0, carry_addition = 0;
-        for (j = length_num1 - 1; j >= 0; j--)
-        {
-            result_index = i + j + 1;
-            multiplication = (argv[1][j] - '0') * (argv[2][i] - '0') + carry_multiply;
-            carry_multiply = multiplication / 10;
-            addition = (result[result_index] - '0') + (multiplication % 10) + carry_addition;
-            carry_addition = addition / 10;
-            result[result_index] = (addition % 10) + '0';
-        }
-        result[result_index - 1] = (carry_multiply + carry_addition) + '0';
-    }
-
-    if (result[0] == '0')
-        leading_zeros = 1;
-
-    for (; leading_zeros < total_length; leading_zeros++)
-    {
-        printf("%c", result[leading_zeros]);
-    }
-    printf("\n");
-
-    free(result);
-    return 0;
+	return (0);
 }
-
