defa88673e069980fe2c734f11d4e9c804279655
Astambar <leuchartjason@gmail.com>
2023-09-22 21:38:14 +0000

    making change

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -5,13 +5,19 @@ Main file for testing
 
 
 def makeChange(coins, total):
-    if total < 0:
-        return -1
+    if total <= 0:
+        return 0
 
-    # Initialiser un tableau pour stocker le nombre minimum de pièces pour chaque montant
+    for coin in coins:
+        if coin <= 0:
+            return -1
+
+    # Initialiser un tableau pour stocker le nombre minimum
+    # de pièces pour chaque montant
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
+    # Calcul du nombre minimum de pièces
     for coin in coins:
         for amount in range(coin, total + 1):
             dp[amount] = min(dp[amount], dp[amount - coin] + 1)
